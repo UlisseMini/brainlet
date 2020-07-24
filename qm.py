7ecb98faa8fba4d3267385d996d425d48d538778
Ulisse Mini <ulisse.mini@gmail.com>
2020-07-24 16:55:26 -0400

    Make more generic, add 11 mult training

--- qm.py
+++ qm.py
@@ -3,9 +3,9 @@ import pickle
 import os
 from typing import Dict
 
-def quiz(questions: Dict[str, str]):
+def quiz(name: str, questions: Dict[str, str]):
     try:
-        num_correct = pickle.load(open('mafs.pkl', 'rb'))
+        num_correct = pickle.load(open(f'{name}.pkl', 'rb'))
     except:
         num_correct = {q: 0 for q in questions}
 
@@ -27,13 +27,19 @@ def quiz(questions: Dict[str, str]):
         print(e)
         pickle.dump(num_correct, open('mafs.pkl', 'wb'))
 
-def main():
+def quiz_times_table():
     questions = {}
     for x in range(3, 10):
         for y in range(3, 10):
             questions[(f'{x} * {y}', f'{y} * {x}')] = str(x * y)
 
-    quiz(questions)
+    quiz('times-tbl', questions)
+
+def quiz_2_digit_by_11():
+    quiz(
+        '2-by-11',
+        {(f'{x} * 11', f'11 * {x}'): str(11*x) for x in range(10, 100)},
+    )
 
 if __name__ == '__main__':
-    main()
+    quiz_2_digit_by_11()
