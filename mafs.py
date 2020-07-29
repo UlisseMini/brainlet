dd81731910539375b58c80804f82f38392b459fc
Ulisse Mini <ulisse.mini@gmail.com>
2020-07-29 13:54:45 -0400

    Rework quiz() archetecture

--- mafs.py
+++ mafs.py
@@ -1,4 +1,5 @@
 import time
+import pathlib
 import csv
 import os.path
 from typing import Iterator
@@ -10,27 +11,27 @@ class Q:
         self.answer = answer if type(answer) == str else str(answer)
 
 
-def quiz(path: str, questions: Iterator[Q]):
-    # write the header if t he fiel does not exist
-    # note if directory this will be sad
-    if not os.path.isfile(path):
-        with open(path, 'w', newline='') as f:
-            w = csv.writer(f)
-            w.writerow(['question', 'answer', 'correct_answer', 'time'])
-
+def quiz(name: str, questions: Iterator[Q], blocksize=20):
+    pathlib.Path(f'data/{name}').mkdir(parents=True, exist_ok=True)
+    path = os.path.join('data', name, f'{time.time():.0f}.csv')
 
     with open(path, 'a', newline='') as f:
         w = csv.writer(f)
+        w.writerow(['question', 'answer', 'correct_answer', 'time'])
 
-        for q in questions:
-            start = time.time()
+        print(f'solve {blocksize} {name} questions!')
+        for i, q in enumerate(questions):
             print(q.question)
+            start = time.time()
             answer = input('-> ')
+            end = time.time()
             if answer == q.answer:
                 print('Correct')
             else:
                 print(f'Wrong, {q.question} was {q.answer}')
 
-            w.writerow([q.question, answer, q.answer, round(time.time() - start, 2)])
+            w.writerow([q.question, answer, q.answer, round(end - start, 2)])
+
+            if i == blocksize-1: break
 
 
--- times_tables.py
+++ times_tables.py
@@ -4,12 +4,16 @@ import random
 def questions() -> mafs.Q:
     while True:
         n1 = random.randint(2, 9)
-        n2 = random.randint(2, 9)
-        yield mafs.Q(f'{n1} * {n2}', n1*n2)
+        n2 = random.randint(6, 9)
+
+        yield random.choice([
+            mafs.Q(f'{n1} * {n2}', n1*n2),
+            mafs.Q(f'{n2} * {n1}', n1*n2),
+        ])
 
 
 def main():
-    mafs.quiz('data/tt.csv', questions())
+    mafs.quiz('times tables', questions(), 100)
 
 
 if __name__ == '__main__':
